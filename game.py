c7b8328a2613a800d984dd0ee84c50661f2a763b
ariadne10 <66440668+ariadne10@users.noreply.github.com>
2023-06-05 21:11:39 -0400

    Update game.py

--- game.py
+++ game.py
@@ -24,9 +24,11 @@ def get_winners_and_losers(df):
     return winners, losers
 
 def add_early_payments(winners, early_moneyZ, early_moneyA):
+    print(winners)  # print the winners before appending
     winners = winners.append({'Agent':'Zane','This Week':early_moneyZ}, ignore_index=True)
     winners = winners.append({'Agent':'Austin','This Week':early_moneyA}, ignore_index=True)
     return winners
+
 
 def generate_payments(winners, losers):
     n_winners = winners.shape[0]
